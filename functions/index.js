96f34aafe080042f7988ba87727bc8a7975619df
skyhunter12 <akashbagali043@gmail.com>
2025-05-01 16:09:03 +0530

    hosting changes for path

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,12 @@
 import * as functions from "firebase-functions";
 import next from "next";
-import path from "path";
-import { fileURLToPath } from "url";
+// import path from "path";
+// import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-const app = next({ dev: false,dir:__dirname, conf: { distDir: ".next" } });
+const app = next({ dev: false, conf: { distDir: ".next" } });
 const handle = app.getRequestHandler();
 
 export const nextApp = functions.https.onRequest(async (req, res) => {
