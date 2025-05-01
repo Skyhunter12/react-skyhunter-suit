796d2064586b0d0f08509bf00c8ab1def7688cd2
skyhunter12 <akashbagali043@gmail.com>
2025-05-01 10:59:22 +0530

    deploy server side to client

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   trailingSlash: true,
-   output: 'export',
   eslint:{
     ignoreDuringBuilds:true
   }
